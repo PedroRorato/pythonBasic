b5200ff79e6a2cdbd5372be2f041355aec8e20b4
Pedro <pedroberorato@gmail.com>
2021-01-26 19:40:28 -0300

    First1

--- Ch2/hello_world.py
+++ Ch2/hello_world.py
@@ -1,1 +1,10 @@
-print("Hello World");
+#First Code
+
+def main():
+    print("Hello World")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+
+if __name__ == "__main__":
+    main()
